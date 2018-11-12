9c49ad335567d7a28f2f040c9e5c7960460580e1
Willie Shubert <willie.pictograms@gmail.com>
2018-11-12 10:50:20 -0800

    Prep files for hosting on S3

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ var top10Bars = new Top10Bars({
     parentId: 'chart',
 });
 
-d3.json('data/top10.json', function (error, data) {
+d3.json('https://s3.amazonaws.com/mongabay-imgs/madera_sucia_vz/data/top10.json', function (error, data) {
     if (error) throw error;
 
-    d3.csv('data/empresas.csv', function (error, data_empresas) {
+    d3.csv('https://s3.amazonaws.com/mongabay-imgs/madera_sucia_vz/data/empresas.csv', function (error, data_empresas) {
         if (error) throw error;
 
         top10Bars.loadData(data);
